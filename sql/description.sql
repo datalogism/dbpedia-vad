f0f46ddd3dc8a54f66a60279c4fb440936b4cf1a
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-27 10:39:11 +0100

    Update description.sql

--- sql/description.sql
+++ sql/description.sql
@@ -521,17 +521,27 @@ create procedure dbp_ldd_split_url (in uri varchar, out pref varchar, out res va
 
 create procedure dbp_ldd_get_proxy (in x varchar)
 {
-  if (x like 'nodeID://%')
+     if (x like 'nodeID://%')
+
     return '/about/html/' || x;
-  if (x like 'http://dbpedia.org/%' and 'fr.dbpedia.org' <> 'dbpedia.org')
-    return regexp_replace (x, 'http://dbpedia.org', 'http://'||'fr.dbpedia.org');
-  if (x like registry_get('dbp_domain') || '/%' and 'fr.dbpedia.org' <> replace(registry_get('dbp_domain'),'http://',''))
-    return regexp_replace (x, registry_get('dbp_domain'), 'http://'||'fr.dbpedia.org');
+
+  if (x like 'http://dbpedia.org/%' and http_request_header (http_request_header (), 'Host') <> 'dbpedia.org')
+
+    return regexp_replace (x, 'http://dbpedia.org', 'http://'||http_request_header (http_request_header (), 'Host'));
+
+  if (x like registry_get('dbp_domain') || '/%' and http_request_header (http_request_header (), 'Host') <> replace(registry_get('dbp_domain'),'http://',''))
+
+    return regexp_replace (x, registry_get('dbp_domain'), 'http://'||http_request_header (http_request_header (), 'Host'));
+
 
   if (connection_get ('mappers_installed') = 1 and (
+
       x like 'http://www.w3.org/2002/07/owl%' or
+
       x like 'http://www.w3.org/1999/02/22-rdf-syntax-ns%' or
+
       x like 'http://www.w3.org/2000/01/rdf-schema%'))
+
     return '/about/html/' || replace (x, '#', '%01');
 
   return x;
